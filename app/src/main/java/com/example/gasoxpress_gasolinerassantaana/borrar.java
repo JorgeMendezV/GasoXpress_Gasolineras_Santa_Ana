8362423ecc23d17b58f9417eb24881f4674238e4
JorgeMendezV <jorgemendez3421@gmail.com>
2022-11-18 22:43:29 -0600

    Logica iniciada para boton borrar

--- app/src/main/java/com/example/gasoxpress_gasolinerassantaana/borrar.java
+++ app/src/main/java/com/example/gasoxpress_gasolinerassantaana/borrar.java
@@ -14,5 +14,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class borrar extends AppCompatActivity {
+    private List<Datos> items;
+    //Metodo para borrar los datos a la tabla de gasoxpress
+    public void deleteItem(View view, int i) {
+        Datos item=items.get(i);
+        abrirDB base= new abrirDB(this,"gasolinerasT",null,1);
+        SQLiteDatabase db = base.getWritableDatabase();
+        ContentValues id = new ContentValues();
+        id.get("_id");
+        db.delete("gasoxpress", id + " = ?",
+                new String[]{String.valueOf(item.getId())});
+        db.close();
+        Toast.makeText(this,"Se elimino correctamente", Toast.LENGTH_SHORT).show();
+        System.out.println("Intentando eliminar");
+    }
+
+    public void deleteItem(Object item) {
+        listArray.remove(item);
+        notifyDataSetChanged();
+
+    }
 
 }
